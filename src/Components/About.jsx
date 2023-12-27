0d88fe6defad2109ab82fb995693a8b20569c20a
Preet0997 <Preet.Shah@studentambassadors.com>
2023-12-27 17:47:02 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CS student studying at NMIMS University. I passionate about technology and its immense potential to change the world";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web designing",
+  "Artificial Intelligence",
+  "Machine Learning",
+  "Augmented Reality",
+  "Virtual Reality",
+  "Aerospace",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Keen Learner and Explorer";
 
 const About = () => {
   return (
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/yayy.jpg";
+import image from "../images/Yayy.jpg";
 
 const imageAltText = "Preet Shah - AIML Researcher";
 
